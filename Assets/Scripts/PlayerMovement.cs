e948e499eabb69862daea948ce87128a31fc0346
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-06-22 02:36:59 +0300

    added ground check + changed moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,55 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed = 5f;
+    public float maxSpeed = 5f;
     public float jumpForce = 5f;
+
+    Vector2 inputVector;
+
+    bool grounded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate()
+    {
+        if (inputVector.x != 0)
+        {
+            if (Mathf.Abs(rb.linearVelocity.x) < maxSpeed)
+            {
+                rb.AddForce(new Vector2(inputVector.x * speed, 0f), ForceMode2D.Force);
+            }
+        }
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 inputVector = context.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector2(inputVector.x * speed, rb.linearVelocity.y);
+        inputVector = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && grounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            grounded = false;
         }
     }
 }
