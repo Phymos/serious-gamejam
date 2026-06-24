711a34f93247f0b46f8613766b9a35a17e8aeb9b
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-06-25 01:04:36 +0300

    changed danger to trigger

--- Assets/Scripts/Danger.cs
+++ Assets/Scripts/Danger.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class Danger : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
