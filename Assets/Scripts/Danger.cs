c20957e078a0c48e5bfc6721acfc68f381d21b46
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-06-27 00:02:33 +0300

    added death sound

--- Assets/Scripts/Danger.cs
+++ Assets/Scripts/Danger.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class Danger : MonoBehaviour
 {
+    public AudioSource playerAudio;
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if (playerAudio != null)
+            {
+                playerAudio.PlayOneShot(playerAudio.clip);
+            }
+
+            Invoke("RestartScene", 0.3f);
         }
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
