8163489428df57bf1afe07ac7aef65f5b0742372
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-06-26 15:24:23 +0300

    deleted unnecesary parts

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -3,19 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void NextLevelFunc()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
